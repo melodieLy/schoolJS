7458a725aeb3eacd997bd0cae3ebaa4d0c8a0b2b
mly <mly@ongconseil.com>
2019-10-21 10:14:31 +0200

    Initialization for the exercice 1 for 10/25

---
+++ exo1.js
@@ -0,0 +1,12 @@
+// TO DO : Make a DeepClone Function (use recursion) for 
+// - array
+// - object
+// - date 
+// - function
+/*When performing a deep copy, those external objects are copied as well, so the new, cloned object is completely independent from the old one.*/
+
+function DeepClone( ) {
+    return DeepClone()
+}
+
+DeepClone();
